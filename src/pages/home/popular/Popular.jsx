71eca85c7aa963ff777a140d2e2d1063b7b5a503
Aashutosh Gandhi <aashutosh@Aashutoshs-MacBook-Air.local>
2024-06-16 22:37:22 +0530

    web development project 1 is completed now

---
+++ src/pages/home/popular/Popular.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/Carousel/Carousel";
+const Popular = () => {
+  const [endpoint, setEndpoint] = useState("movie");
+  const { data, loading } = useFetch(`/${endpoint}/popular`);
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Popular</span>
+        <SwitchTabs data={["Movies", "TV Shoes"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
+    </div>
+  );
+};
+export default Popular;
